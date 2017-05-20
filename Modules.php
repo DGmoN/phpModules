dea460c760af67d8e74966153f8597c53bcc3eac
DGmoN <DGmon.mail@gmail.com>
2017-05-20 08:59:28 -0700

    Added root config

--- Modules.php
+++ Modules.php
@@ -44,15 +44,14 @@ if(file_exists($__MODULE_CONFIG)){
 	foreach($php_modules_config as $st){
 		__APPEND_LOG("Module loading: ".$st->MODULEName);
 		
-		$module_class_file = $st->MODULESrc."module.php";
-		
+		$module_class_file = $MODULES_ROOT.'/'.$st->MODULESrc."module.php";
 		if(file_exists($module_class_file)){
 			require_once($module_class_file);
 			$cName = $st->MODULEName."Module";
 			$__MODULE_REGISTRY[$st->MODULEName] = new $cName($st);
 			__APPEND_LOG("Module ok: ".$st->MODULEName);
 		}else{
-			__APPEND_LOG("Module loading FAILED: ".$st->MODULEName);
+			__APPEND_LOG("Module loading FAILED: ".$module_class_file);
 		}
 			
 	}
